95aec75f21da8d351ffc2d4041f9cb1fa2082d16
A.Ali <ali.a@aucegypt.edu>
2025-01-24 14:29:09 +0200

    Initialize problem 2

---
+++ problem2/P2-palindrome.cpp
@@ -0,0 +1,17 @@
+/*
+Given an integer x, return true if x is a 
+palindrome, and false otherwise.
+
+palindrome is when the integer is read the same forward and backward
+
+Example : 
+. 121 ----> palindrome
+. 122 ----> Not palindrome
+
+Constraints:
+
+Negative numbers are not palindromes.
+
+Numbers ending with 0 (except 0 itself) are not palindromes.
+*/
+
---
+++ problem2/refreshCompareStrings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+
+int main () {
+  //  //Refresh for comparing strings
+// const char *string1 = "match";
+// const char *string2 = "Match";
+
+// if (strcmp(string1, string2) == 0 ) 
+//     cout << "\n both matched" << endl;
+// else 
+//     cout << "Not matched" << endl;
+
+
+// // Reversing a String 
+
+
+// string str = "Let's go";
+// reverse(str.begin(), str.end());
+// cout<< str;
+
+
+// commbine both
+
+
+
+
+return 0;
+}
