f19ccdd51c67f0ac037e379fdf3898b57b719b90
A.Ali <ali.a@aucegypt.edu>
2025-01-25 10:00:15 +0200

    second algorithm! well that's definetly better

--- problem2/P2-palindrome.cpp
+++ problem2/P2-palindrome.cpp
@@ -64,7 +64,21 @@ public:
 
 
     /* We want something more efficient from a meomry wise*/
+    // second algorithm
 
+    if (x < 0) return false;
+    if (x % 10 == 0 && x != 0) return false;
+
+
+    int reversed_num = 0;
+    while (x > reversed_num) {
+        reversed_num = reversed_num * 10 + x % 10;
+        x = x /10;
+
+    
+    }
+
+    return x == reversed_num || x == reversed_num / 10;
     
 
 
