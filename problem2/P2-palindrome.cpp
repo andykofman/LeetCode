6cfa243de963317505073d90537f0dda1fd50aa8
A.Ali <ali.a@aucegypt.edu>
2025-01-25 09:31:15 +0200

    alternative loop; more simple one. I think!

--- problem2/P2-palindrome.cpp
+++ problem2/P2-palindrome.cpp
@@ -15,6 +15,8 @@ Negative numbers are not palindromes.
 Numbers ending with 0 (except 0 itself) are not palindromes.
 */
 
+//Solution no.1
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -23,29 +25,48 @@ class Solution {
 
 public: 
     bool isPalindrome (int x) {
-        // Handle edge casses
-        // negative number ----> false
-        if (x < 0 ) return false;
-        //  ends with zero ----> false
-        // if (x % 10 == 0 ) return false;              removed because it will be included in the loop
+    //     // Handle edge casses
+    //     // negative number ----> false
+    //     if (x < 0 ) return false;
+    //     //  ends with zero ----> false
+    //     // if (x % 10 == 0 ) return false;              removed because it will be included in the loop
 
-        string s = to_string (x);
-        int n = s.length();
+    //     string s = to_string (x);
+    //     int n = s.length();
 
-        //you only need to compare the first half of the string with 
-        //the second half to check for symmetry
+    //     //you only need to compare the first half of the string with 
+    //     //the second half to check for symmetry
 
-        for (int i= 0; i < n/2; i++) {
-            if (s[i] != s[n-1-i] ) {
-                return false;
 
-            }
-           
-        }
 
-    return true;
+
+    //     for (int i= 0; i < n/2; i++) {
+    //         if (s[i] != s[n-1-i] ) {
+    //             return false;
+
+    //         }
+
+    //     // Alternative loop
+    //         // int right = 0;
+    //         // int left = s.length()-1;    
+
+    //         // while (right < left){
+    //         //     if ( s [left] != s [right]) {
+    //         //         return false;
+    //         //     }
+    //         // return true;
+
+    //         // }
+    //     }
+
+    // return true;
+    // }
+
+
+    /* We want something more efficient from a meomry wise*/
+
+    
+
+
     }
-
-
-
 };
