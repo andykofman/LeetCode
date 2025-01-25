c2beb9026e159df51e43475c451e04e18c63b669
A.Ali <ali.a@aucegypt.edu>
2025-01-25 05:06:26 +0200

    first solution

--- problem2/P2-palindrome.cpp
+++ problem2/P2-palindrome.cpp
@@ -15,3 +15,37 @@ Negative numbers are not palindromes.
 Numbers ending with 0 (except 0 itself) are not palindromes.
 */
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+
+public: 
+    bool isPalindrome (int x) {
+        // Handle edge casses
+        // negative number ----> false
+        if (x < 0 ) return false;
+        //  ends with zero ----> false
+        // if (x % 10 == 0 ) return false;              removed because it will be included in the loop
+
+        string s = to_string (x);
+        int n = s.length();
+
+        //you only need to compare the first half of the string with 
+        //the second half to check for symmetry
+
+        for (int i= 0; i < n/2; i++) {
+            if (s[i] != s[n-1-i] ) {
+                return false;
+
+            }
+           
+        }
+
+    return true;
+    }
+
+
+
+};
